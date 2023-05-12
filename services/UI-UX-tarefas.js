448c8a6e7d621b872f9489d6974786d5c9a5c47e
Luiz Marangoni <lzmarangoni@gmail.com>
2023-05-12 17:28:11 -0300

    transformação das funcoes em assincronas

--- services/UI-UX-tarefas.js
+++ services/UI-UX-tarefas.js
@@ -2,17 +2,16 @@
 const axios = require('axios')
 
 
-function getTasksUI(){
+async function getTasksUI(){
   
   // return JSON.parse(fs.readFileSync("tarefas.json"))
-
-    axios.get("https://api.clickup.com/api/v2/list/217098040/task",
+  const response = await axios.get("https://api.clickup.com/api/v2/list/217098040/task",
     {headers:{"Authorization":"pk_60976530_RQQUNMS67QZV31TXG2L5M6HTRTH4BK14"}})
-  .then(res =>  console.log(res.data))
-  
+
+  return response.data
 }
     
-getTasksUI()
+
 
 
 
