2a0b74f15ba16488f40a1da3ea0ccfed4844951a
Luiz Marangoni <lzmarangoni@gmail.com>
2023-05-15 17:00:04 -0300

    inclusa endpoint de tasks filtrada pelo team

--- services/UI-UX-tarefas.js
+++ services/UI-UX-tarefas.js
@@ -3,9 +3,9 @@ const axios = require('axios')
 
 
 async function getTasksUI(){
-  
+  const teamID = "36953942"
   // return JSON.parse(fs.readFileSync("tarefas.json"))
-  const response = await axios.get("https://api.clickup.com/api/v2/list/217098040/task",
+  const response = await axios.get("https://api.clickup.com/api/v2/team/36953942/task",
     {headers:{"Authorization":"pk_60976530_RQQUNMS67QZV31TXG2L5M6HTRTH4BK14"}})
 
   return response.data
