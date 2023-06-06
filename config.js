7aae99022790d70276a00b425ba800569c8cb135
Luiz Marangoni <lzmarangoni@gmail.com>
2023-06-06 13:38:51 -0300

    inclusao de nova requisição para identificar cada id de cada usuario, sera usado para fazer as requisições de time tracking por usuário.

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 
     const urlTasksTeam = "https://api.clickup.com/api/v2/team/36953942/task?page="
     const urlTaskTime = "https://api.clickup.com/api/v2/team/36953942/time_entries?"
+    const urlMembers = "https://api.clickup.com/api/v2/team"
     const credencials =  {headers:{
         "Authorization":"pk_60976530_RUGFYYWSIZB124CCIE655Z2Q22W0S2CH",
         "Content-Type":"application/json", 
          
     }}
 
-   module.exports = {urlTaskTime, urlTasksTeam, credencials}
+   module.exports = {urlTaskTime, urlTasksTeam, urlMembers, credencials}
--- services/getTasks.js
+++ services/getTasks.js
@@ -1,13 +1,14 @@
 const axios = require('axios')
-const { urlTasksTeam, credencials } = require("../config")
+const { urlTasksTeam, urlMembers, credencials } = require("../config")
 
 async function getTasks() {
   let allTasks = []
   let pages = 0
-  
+  /*
     while(true){
     let url = urlTasksTeam + pages 
     let response = await axios.get(url, credencials)
+    
     const tasks = response.data.tasks
 
     if (tasks.length === 0){
@@ -18,10 +19,12 @@ async function getTasks() {
     pages++
     console.log(url)
     }
+    */
+    const responseUsers = await axios.get(urlMembers, credencials)
+    const data = responseUsers.data.teams
+    const membersID = data.map(member => member.members.map(user => user.user.id))
+    console.log(membersID)
     
-
-
-
 return allTasks
 }
 
